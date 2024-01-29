33277cd7a568ca53e5c71f674b62f017d8256ca1
PaidXSmall <128967985+PaidXSmall@users.noreply.github.com>
2024-01-29 21:53:42 +0530

    Update basic.py

--- StringSessionBot/basic.py
+++ StringSessionBot/basic.py
@@ -19,6 +19,19 @@ cpu_usage = psutil.cpu_percent()
 ram_usage = psutil.virtual_memory().percent
 disk_usage = psutil.disk_usage('/').percent
 
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+    
 START_TEXT = """
 <b> Hᴇʟʟᴏ {}
 
