f9b1a0dd99906c0cb4c760073bd48bd681178739
PaidXSmall <128967985+PaidXSmall@users.noreply.github.com>
2024-01-31 12:18:15 +0530

    Update basic.py

--- StringSessionBot/basic.py
+++ StringSessionBot/basic.py
@@ -94,6 +94,8 @@ OWNER_TEXT = """
 
 START_BUTTONS = InlineKeyboardMarkup(
         [[
+        InlineKeyboardButton("🔥Sᴛᴀʀᴛ Gᴇɴᴇʀᴀᴛɪɴɢ Sᴇssɪᴏɴ 🔥", callback_data="generate")
+        ],[
         InlineKeyboardButton('✇ Uᴘᴅᴀᴛᴇs ', url=f"https://t.me/PeterXCLouD"),
         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ ⌬', url=f"https://t.me/PeterXCLouD")
         ],[
@@ -163,9 +165,40 @@ async def cb_data(bot, update):
             disable_web_page_preview=True,
             reply_markup=OWNER_BUTTONS
         )
+    elif query == "generate":
+        await callback_query.answer()
+        await callback_query.message.reply(ask_ques, reply_markup=InlineKeyboardMarkup(buttons_ques))
+    elif query.startswith("pyrogram") or query.startswith("telethon"):
+        try:
+            if query == "pyrogram":
+                #await callback_query.answer("Please note that the new type of string sessions may not work in all bots, i.e, only the bots that have been updated to pyrogram v2 will work!", show_alert=True)
+                await generate_session(bot, callback_query.message)
+                """
+            # Maybe in future it'll come back.
+            elif query == "pyrogram1":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, old_pyro=True)
+                """
+            elif query == "pyrogram_bot":
+                await callback_query.answer("Please note that this bot session will be of pyrogram v2", show_alert=True)
+                await generate_session(bot, callback_query.message, is_bot=True)
+            elif query == "telethon_bot":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
+            elif query == "telethon":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, telethon=True)
+        except Exception as e:
+            print(traceback.format_exc())
+            print(e)
+            await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
     else:
         await update.message.delete()
-        
+
+ERROR_MESSAGE = "Oops! An exception occurred! \n\n**Error** : {} " \
+            "\n\nPlease visit @StarkBotsChat if this message doesn't contain any " \
+            "sensitive information and you if want to report this as " \
+            "this error message is not being logged by us!"
 # Start Message
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
